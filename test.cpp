d5fb15221669775d8be807cd55d656117639aa7a
Redefinition0726 <redefinition0726@163.com>
2023-07-12 11:50:52 +0800

    by Redefinition0726

---
+++ test.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+const int _1e2 = 1e2 + 5;
+const int _2e2 = 2e2 + 5;
+const int _1e3 = 1e3 + 5;
+const int _2e3 = 2e3 + 5;
+const int _1e5 = 1e5 + 5;
+const int _2e5 = 2e5 + 5;
+const int _inf = 0x3f3f3f3f;
+int p[_2e5];
+int main()
+{
+    return 0;
+}
