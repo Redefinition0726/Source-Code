55558f3ad06cb01c322edfc1f42504954f1d1fe8
Redefinition0726 <redefinition0726@163.com>
2023-07-13 09:47:38 +0800

    by Redefinition0726

--- Luogu/P4779.cpp
+++ Luogu/P4779.cpp
@@ -22,46 +22,7 @@
 #define debug printf("Debug\n")
 typedef long long ll;
 const int inf = 0x3f3f3f3f;
-const int MaxN = 1e5 + 5;
-const int MaxM = 2e5 + 5;
-int n, m;
-int start;
-bool f[MaxN];
-int x, y, len;
-int answer[MaxN];
-std::vector<int> link[MaxN];
-void dijkstra(int x, int len)
-{
-    std::queue<int> bfs;
-    if (f[x])
-    {
-        return;
-    }
-    f[x] = true;
-    for (int i = 0; i < link[x].size(); i++)
-    {
-        int to = link[x][i];
-        if (!f[to])
-        {
-            answer[i] = link[x][i];
-            bfs.push(link[x][i]);
-        }
-    }
-}
 int main()
 {
-    scanf("%d%d%d", &n, &m, &start);
-    for (int i = 1; i <= m; i++)
-    {
-        scanf("%d%d%d", &x, &y, &len);
-        link[x].push_back(len);
-        link[y].push_back(len);
-    }
-    dijkstra(start, 0);
-    for (int i = 1; i <= n; i++)
-    {
-        printf("%d ", answer[i]);
-    }
-    printf("\n");
     return 0;
 }
