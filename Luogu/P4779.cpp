205fd3247e5e1f22b13cca7b3ebdb5966de32e7d
Redefinition0726 <redefinition0726@163.com>
2023-07-13 10:30:36 +0800

    by Redefinition0726

--- Luogu/P4779.cpp
+++ Luogu/P4779.cpp
@@ -22,7 +22,42 @@
 #define debug printf("Debug\n")
 typedef long long ll;
 const int inf = 0x3f3f3f3f;
+const int MaxN = 1e5 + 5;
+const int MaxM = 2e5 + 5;
+struct str
+{
+    int x, y;
+    int len;
+};
+int n, m;
+int start;
+int x, y, len;
+int answer[MaxN];
+std::vector<std::pair<int, int>> link[MaxN];
+std::priority_queue<std::pair<int, int>> queue;
 int main()
 {
+    scanf("%d%d%d", &n, &m, &start);
+    for (int i = 1; i <= m; i++)
+    {
+        scanf("%d%d%d", &x, &y, &len);
+        link[x].push_back(std::make_pair(y, len));
+        link[y].push_back(std::make_pair(x, len));
+    }
+    memset(answer, 0x3f, sizeof(answer));
+    queue.push(std::make_pair(0, start));
+    for (; !queue.empty();)
+    {
+        std::pair<int, int> tmp;
+        tmp = queue.top();
+        queue.pop();
+        for (int i = 0; i < link[tmp.second].size(); i++)
+        {
+            if (answer[tmp.second])
+            {
+                ;
+            }
+        }
+    }
     return 0;
 }
